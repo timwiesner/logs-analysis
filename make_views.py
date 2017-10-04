bd1ff3092d74ad1c87b72afd04c2d55b6cee22a5
timwiesner <timothyjwiesner@gmail.com>
2017-10-04 16:04:27 -0500

    fix: Indentation

--- make_views.py
+++ make_views.py
@@ -5,22 +5,20 @@ import psycopg2
 DBNAME = "news"
 
 
-def query_one():
+def topthree_view():
     """Return relevant queries from the database."""
     conn = psycopg2.connect(database=DBNAME)
     cur = conn.cursor()
-    cur.execute("SELECT \
-                    articles.title, \
-                    topthree.count \
-                FROM topthree \
-                JOIN articles \
-                    ON topthree.substring = articles.slug \
-                ORDER BY count DESC")
+    cur.execute(
+        "CREATE VIEW topthree AS \
+         SELECT SUBSTRING(path, 10), COUNT(*) \
+         FROM log \
+         WHERE path LIKE '/article/%' \
+         GROUP BY path \
+         ORDER BY count DESC \
+         LIMIT 3")
     rows = cur.fetchall()
-    print("Top Three Articles:")
-    for row in rows:
-        print(row)
     cur.close()
     conn.close()
 
-query_one()
+topthree_view()
