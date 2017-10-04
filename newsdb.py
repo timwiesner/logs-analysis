bcc2a402ff37ef83acf8e27457974486b77bf372
timwiesner <timothyjwiesner@gmail.com>
2017-10-04 15:51:14 -0500

    style: Remove unused code

--- newsdb.py
+++ newsdb.py
@@ -9,12 +9,6 @@ def query_one():
     """Return relevant queries from the database."""
     conn = psycopg2.connect(database=DBNAME)
     cur = conn.cursor()
-    # cur.execute("SELECT SUBSTRING(path, 10), COUNT(*) \
-    #     FROM log \
-    #     WHERE path LIKE '/article/%' \
-    #     GROUP BY path \
-    #     ORDER BY count DESC \
-    #     LIMIT 3")
     cur.execute("SELECT \
                     articles.title, \
                     topthree.count \
