b35b368ae347ebff8b9d660f3b304d8072bdcbf1
timwiesner <timothyjwiesner@gmail.com>
2017-10-05 14:12:35 -0500

    feat: Begin query two

--- queries.py
+++ queries.py
@@ -20,6 +20,33 @@ def query_one():
     for row in rows:
         print(row)
     cur.close()
+    print('\n')
     conn.close()
 
+
+def query_two():
+    """Return top three accessed articles from the database."""
+    conn = psycopg2.connect(database="news")
+    cur = conn.cursor()
+    cur.execute("SELECT authors.name, articles.title \
+                 FROM authors \
+                 LEFT JOIN articles \
+                    ON authors.id = articles.author")
+    rows = cur.fetchall()
+    print("Top Three Articles:")
+    for row in rows:
+        print(row)
+    cur.close()
+    print('\n')
+    conn.close()
+
+
+
+
+
+
+
+print('\n')
+
 query_one()
+query_two()
