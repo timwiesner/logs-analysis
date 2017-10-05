f0d3c908e6515cf1d061bf9f6d268a5ad91f01d7
timwiesner <timothyjwiesner@gmail.com>
2017-10-05 10:32:17 -0500

    feat: Limit query to top three

--- queries.py
+++ queries.py
@@ -8,12 +8,13 @@ def query_one():
     conn = psycopg2.connect(database="news")
     cur = conn.cursor()
     cur.execute("SELECT \
-                 articles.title, \
-                 topthree.count \
-                 FROM topthree \
+                    articles.title, \
+                    most_accessed.count \
+                 FROM most_accessed \
                  JOIN articles \
-                    ON topthree.substring = articles.slug \
-                 ORDER BY count DESC")
+                 ON most_accessed.substring = articles.slug \
+                 ORDER BY count DESC \
+                 LIMIT 3")
     rows = cur.fetchall()
     print("Top Three Articles:")
     for row in rows:
