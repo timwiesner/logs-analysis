ec63334aafb8c49fc21332ada87c7321ea57414b
timwiesner <timothyjwiesner@gmail.com>
2017-10-06 11:00:49 -0500

    style: Reformat query one

--- queries.py
+++ queries.py
@@ -7,7 +7,13 @@ def query_one():
     """Return top three accessed articles from the database."""
     conn = psycopg2.connect(database="news")
     cur = conn.cursor()
-    cur.execute("SELECT CONCAT(initcap(articles.title), ' - ', count(*), ' views') FROM log JOIN articles ON substring(path, 10) = articles.slug GROUP BY articles.title ORDER BY log.count DESC LIMIT 3")
+    cur.execute(
+        "SELECT \
+            CONCAT(initcap(articles.title), ' - ', count(*), ' views') \
+        FROM log \
+        RIGHT JOIN articles \
+            ON substring(path, 10) = articles.slug \
+            GROUP BY articles.title ORDER BY log.count DESC LIMIT 3")
     rows = cur.fetchall()
     print("Top Three Articles:")
     for row in rows:
@@ -21,13 +27,15 @@ def query_two():
     """Return most popular article authors."""
     conn = psycopg2.connect(database="news")
     cur = conn.cursor()
-    cur.execute("SELECT CONCAT \
-                    (authors.name, ' - ', SUM(most_popular.count), ' views') \
-                 FROM most_popular \
-                 RIGHT JOIN authors \
-                    ON most_popular.author = authors.id \
-                GROUP BY authors.name \
-                ORDER BY SUM(most_popular.count) DESC;")
+    cur.execute(
+        "SELECT \
+            CONCAT (authors.name, ' - ', \
+                SUM(most_popular.count), ' views') \
+        FROM most_popular \
+        RIGHT JOIN authors \
+            ON most_popular.author = authors.id \
+        GROUP BY authors.name \
+        ORDER BY SUM(most_popular.count) DESC;")
     rows = cur.fetchall()
     print("Most Popular Article Authors:")
     for row in rows:
@@ -41,7 +49,12 @@ def query_three():
     """Return days where more than 1% of requests lead to errors."""
     conn = psycopg2.connect(database="news")
     cur = conn.cursor()
-    cur.execute("SELECT CONCAT(to_char(time, 'MM-DD-YYYY'), ' - ', status, ': ', COUNT(*), ' requests') FROM log GROUP BY to_char(time, 'MM-DD-YYYY'), status")
+    cur.execute(
+        "SELECT \
+            CONCAT(to_char(time, 'MM-DD-YYYY'), ' - ', status, ': ', \
+                COUNT(*), ' requests') \
+        FROM log \
+        GROUP BY to_char(time, 'MM-DD-YYYY'), status")
     rows = cur.fetchall()
     print("Requests:")
     for row in rows:
@@ -54,5 +67,5 @@ def query_three():
 print('\n')
 
 query_one()
-# query_two()
+query_two()
 # query_three()
